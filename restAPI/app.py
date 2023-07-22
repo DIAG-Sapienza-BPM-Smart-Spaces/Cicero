d0b09102946d01e8c3cf1f3d8ae6886d1721e368
Gianluca Pulicati <gianluca.pulicati@gmail.com>
2023-07-22 19:27:25 +0200

    Working Server Enpoint - basic GET/POST

--- restAPI/app.py
+++ restAPI/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template, make_response
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import json
+import sys
 
 app = Flask(__name__)
 
@@ -14,9 +15,6 @@ current_id = 0
 
 if __name__ == '__main__':
     # app.run(host="localhost", port=8000, debug=True)
-    # reset variables at server start/restart
-    predictions.clear()
-    current_id = 0
     app.run(debug=True)
 
 @app.route('/')
@@ -26,23 +24,36 @@ def welcome():
 
 @app.route('/predictions', methods=['GET', 'POST', 'DELETE'])
 def get_predictions():
+    global predictions
+    global current_id
     if request.method == 'GET':
         resp = make_response(jsonify(predictions), 200)
+        resp.headers.add('Access-Control-Allow-Origin', '*')
         resp.mimetype = 'application/json'
         return resp
     elif request.method == 'POST':
         content_type = request.headers.get('Content-Type')
         if (content_type == 'text/plain'):
-            input_request= str(request.get_data);
+            input_request= request.data.decode('UTF-8')
+            #DEBUG print(input_request, file=sys.stderr)
 
             # predict the result
             input_ids = tokenizer.encode(input_request, return_tensors='pt').to(device)
             output = model.generate(input_ids=input_ids, max_length=150, do_sample=True)
+            generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
             # save the prediction result
-            predictions[++current_id] = output
+            current_id += 1
+            predictions[current_id] = str(generated_text)
+            #DEBUG print("L'id è: " + str(current_id), file=sys.stderr)
+            #DEBUG print("La prediction è: " + str(generated_text), file=sys.stderr)
+            #DEBUG print("Nel database c'è: " + predictions[current_id], file=sys.stderr)
+
             # send back the id to let the client inspect the result
-            return current_id, 201
+            resp = make_response(str(current_id), 201)
+            resp.headers.add('Access-Control-Allow-Origin', '*')
+            resp.mimetype = 'text/plain'
+            return resp
 
         else:
             return "Content type is not supported.", 400
@@ -55,12 +66,17 @@ def get_predictions():
         return "An error has occurred", 501
 
 
-@app.route('predictions/<prediction_id>', methods=['GET', 'DELETE'])
+@app.route('/predictions/<prediction_id>', methods=['GET', 'DELETE'])
 def handle_predictions(prediction_id):
+    global predictions
     if request.method == 'GET':
-        if request.is_json:
+        #if request.is_json:
             #return the predicted text of record "id"
-            return predictions[prediction_id], 200
+            #DEBUG print("Al momento della get, Nel database c'è: " + predictions[int(prediction_id)], file=sys.stderr)
+            resp = make_response(predictions[int(prediction_id)], 200)
+            resp.headers.add('Access-Control-Allow-Origin', '*')
+            resp.mimetype = 'text/plain'
+            return resp
 
     elif request.method == 'DELETE':
         predictions.pop(prediction_id)
