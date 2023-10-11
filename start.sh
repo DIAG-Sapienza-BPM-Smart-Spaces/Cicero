a4252890f6eb30c067cceb51e50eebef5c57fa19
IlKaiser <samuele.rinaldi90@gmail.com>
2023-10-11 13:40:21 +0200

    start script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$volume_name=$1
+
+mkdir $volume_name
+
+docker volume create --driver local --opt type=none --opt device=/raid/mecella/Cicero/$volume_name --opt o=bind $volume_name
+docker build . -t cicero
+docker run -it --gpus all --mount source=$volume_name,target=/code --network=cicero-netw cicero
