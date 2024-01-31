58d8cc8bc784c73a0f1c66387d0c3a450ccdbdbb
IlKaiser <calamo@diag.uniroma1.it>
2024-01-31 14:46:13 +0100

    Update demo with Cicerov2

--- GUI_demo/GUI_sentence_generator.py
+++ GUI_demo/GUI_sentence_generator.py
@@ -3,10 +3,10 @@ import gradio as gr
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Set the device to use
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'mps' #'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load the saved model and tokenizer
-model_path = '../final_model/'
+model_path = 'DIAG-PSSeng/cicero_v2-phi1.5'
 model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
@@ -15,7 +15,7 @@ def generate_text(prompt):
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
 
     # Generate text using the model
-    output = model.generate(input_ids=input_ids, max_length=50, do_sample=True)
+    output = model.generate(input_ids=input_ids, max_length=500, do_sample=True)
 
     # Decode the output to get the generated text
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
