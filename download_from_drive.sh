d8d8aaf29cc8ece5d494c79c28933210348c7a4d
IlKaiser <calamo@diag.uniroma1.it>
2024-01-31 14:47:55 +0100

    Cleanup

--- download_from_drive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-fileid="1EYQnrFpY2vGQFSHaHRcILlxE2drwX8ci"
-filename="ANON_merge23_04.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
--- rm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TARGET=$1
-
-docker run --rm -v $(pwd):/app -w /app alpine chmod -R 777 $TARGET
-rm -fri $TARGET
